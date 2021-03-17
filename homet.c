76b0e4edac6166a7cae4670848c845ebabf75230
umchemurziev <umchemurziev@edu.hse.ru>
2021-03-17 20:16:46 +0300

    12_2

--- homet.c
+++ homet.c
@@ -7,7 +7,7 @@ int main(int argc, char * argv[]) {
     struct stat name;
     for (int i = 0; i < argc - 1; ++i) {
         int id = lstat(argv[i + 1], &name);
-        if (id => 0 || S_ISREG(name.st_mode) || !S_ISLNK(name.st_mode) || name.st_nlink == 1) sum += name.st_size;
+        if (id >= 0 || S_ISREG(name.st_mode) || !S_ISLNK(name.st_mode) || name.st_nlink == 1) sum += name.st_size;
     }
     printf("%llu\n", sum);
 }
