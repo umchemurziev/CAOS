c3128943c398260b145ad50b0705e610cb16b678
umchemurziev <umchemurziev@edu.hse.ru>
2021-03-18 16:12:56 +0300

    sm12_4

--- homet.c
+++ homet.c
@@ -1,13 +1,46 @@
-#include <sys/stat.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 
-int main(int argc, char * argv[]) {
-    long long sum = 0;
-    struct stat name;
-    for (int i = 0; i < argc - 1; ++i) {
-        int id = lstat(argv[i + 1], &name);
-        if (id >= 0 || S_ISREG(name.st_mode) || !S_ISLNK(name.st_mode) || name.st_nlink == 1) sum += name.st_size;
+struct Task
+{
+    unsigned uid;
+    int gid_count;
+    unsigned *gids;
+};
+
+enum { ROOT_ID = 0 };
+enum { USER_SHIFT = 6, GROUP_SHIFT = 3, OTHERS_SHIFT = 0 };
+enum { GRANTED = 1, DENIED = 0 };
+enum { THREE_BITS_MASK = 7U };
+
+static int has_access(unsigned mode, unsigned shift, unsigned request) {
+    mode = (mode >> shift) & THREE_BITS_MASK;
+    request &= THREE_BITS_MASK;
+    if ((mode & request) == request) {
+        return GRANTED;
+    } else {
+        return DENIED;
     }
-    printf("%llu\n", sum);
+}
+
+static int has_group_rights(const struct stat *stb, const struct Task *task) {
+    for (size_t i = 0; i != task->gid_count; ++i) {
+        if (stb->st_gid == task->gids[i]) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int myaccess(const struct stat *stb, const struct Task *task, int access) {
+    if (task->uid == ROOT_ID) {
+        return GRANTED;
+    }
+    if (task->uid == stb->st_uid) {
+        return has_access(stb->st_mode, USER_SHIFT, access);
+    }
+    if (has_group_rights(stb, task)) {
+        return has_access(stb->st_mode, GROUP_SHIFT, access);
+    }
+    return has_access(stb->st_mode, OTHERS_SHIFT, access);
 }
