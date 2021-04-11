2a8d0ede94591b25b3cefd0f7f0f67784596182b
umchemurziev <umchemurziev@edu.hse.ru>
2021-04-11 19:37:55 +0300

    add sm15-2.c

--- sm15/sm15-2.c
+++ sm15/sm15-2.c
@@ -1,5 +1,6 @@
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
@@ -7,14 +8,16 @@
 int main() {
     for (int id = 1; id < 4; ++id) {
         pid_t pid = fork();
-        if (pid) {
-            wait(NULL);
-        } else {
-            int32_t num;
-            scanf("%d", &num);
+        if (!pid) {
+            char str[8];
+            read(0, str, 8);
+            int32_t num = strtol(str, NULL, 10);
             printf("%d %" PRId32 "\n", id, num * num);
             return 0;
         }
+
+        wait(NULL);
+        wait(NULL);
+        wait(NULL);
     }
 }
-
